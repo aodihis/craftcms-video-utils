486a0319ac5d4dd036956e0e59f2cfe3631003f3
Muhammad Iqbal Afif <aodihis@gmail.com>
2022-05-14 22:12:03 +0700

    * Fix vimeo generated url not showing.

--- src/twigextensions/VideoUtilsTwigExtension.php
+++ src/twigextensions/VideoUtilsTwigExtension.php
@@ -119,7 +119,7 @@ class VideoUtilsTwigExtension extends AbstractExtension
 
     public function generateVimeoEmbedUrl($url = null, $noCookie=false): string
     {
-        return 'https://player.vimeo.com/video/'.$this->getVimeoId($url) . $noCookie == 'no-cookie'  ? '?dnt=1' : '';
+        return 'https://player.vimeo.com/video/'.$this->getVimeoId($url) . ($noCookie == 'no-cookie'  ? '?dnt=1' : '');
     }
   
     public function generateVideoEmbedUrl($url = null, $noCookie=false): string
